7278d55830e4f4241f87f1618a6663ac5e619586
e1bandito <aleksandr.shitov@mail.ru>
2021-06-05 14:49:54 +0300

    fix modal overflow

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-require('../js/import/main')
+require('../js/import/main');
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
